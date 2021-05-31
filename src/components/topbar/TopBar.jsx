968acadce7dac9b667a20a774559cc1b22fdf70a
Or Ashtamker <ashi@MacBook-Pro.local>
2021-05-31 18:38:35 +0300

    add nav

---
+++ src/components/topbar/TopBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './topbar.css';
+
+const TopBar = () => {
+    return (
+        <div className="top">
+          nav  
+        </div>
+    )
+}
+
+export default TopBar;
